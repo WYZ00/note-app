2dacc8f8e9b9505113545ea140724459c3aa88d6
WYZ00 <thanthtettin@gmail.com>
2023-07-17 17:34:05 +0630

    DB setup

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,21 @@
+import "dotenv/config";
+import env from "./utils/validateEnv";
+import mongoose from "mongoose";
 import express from "express";
 const app = express();
-const port = 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(port, () => {
-  console.log("Server is running on port:" + port);
-});
+const port = env.PORT;
+
+mongoose
+  .connect(env.MONGO_CONNECTION_STRING)
+  .then(() => {
+    console.log("Mongoose connected");
+    app.listen(port, () => {
+      console.log("Server is running on port:" + port);
+    });
+  })
+  .catch(console.error);
