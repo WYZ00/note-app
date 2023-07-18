5703cff9a73291bf341d591d27bb1b0d225c43e5
WYZ00 <thanthtettin@gmail.com>
2023-07-18 19:42:56 +0630

    finish notes styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
+import styles from "./styles/NotesPage.module.css";
 import { Note as NoteModel } from "./models/note";
 import Note from "./components/Note";
 
@@ -23,11 +25,15 @@ function App() {
   }, []);
 
   return (
-    <div>
-      {notes.map((note) => (
-        <Note note={note} key={note._id} />
-      ))}
-    </div>
+    <Container>
+      <Row xs={1} md={2} xl={3} className="g-4">
+        {notes.map((note) => (
+          <Col key={note._id}>
+            <Note note={note} className={styles.note} />
+          </Col>
+        ))}
+      </Row>
+    </Container>
   );
 }
 
