1caca111f99e82f5b1fa04783419a48b2eb7aadd
WYZ00 <thanthtettin@gmail.com>
2023-07-20 18:59:56 +0630

    User authentication with express-session & cookies

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,6 +4,9 @@ import notesRoutes from "./routes/notes";
 import userRoutes from "./routes/users";
 import morgan from "morgan";
 import createHttpError, { isHttpError } from "http-errors";
+import session from "express-session";
+import env from "./utils/validateEnv";
+import MongoStore from "connect-mongo";
 
 const app = express();
 
@@ -11,9 +14,23 @@ app.use(morgan("dev"));
 
 app.use(express.json());
 
+app.use(
+  session({
+    secret: env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 60 * 60 * 1000,
+    },
+    rolling: true,
+    store: MongoStore.create({
+      mongoUrl: env.MONGO_CONNECTION_STRING,
+    }),
+  })
+);
+
 app.use("/api/users", userRoutes);
 app.use("/api/notes", notesRoutes);
-
 
 app.use((req, res, next) => {
   next(createHttpError(404, "Endpoint not found"));
